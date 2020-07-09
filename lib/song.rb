8fe7caaf7c356cad9f1fc7fcd595bcf4bac165fa
Sami  Sabeeh <samsabeeh6@gmail.com>
2020-07-09 02:16:32 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,12 +2,14 @@ class Song
   attr_accessor :name, :artist, :genre 
   @@count = 0
   @@genres = []
+  @@artists = []
   def initialize(name, artist, genre) 
     @name = name 
     @artist = artist 
     @genre = genre
     @@count += 1
-    @@genres << genre 
+    @@genres << genre
+    @@artists << artist 
   end
   def self.count 
     @@count 
@@ -15,5 +17,8 @@ class Song
   def self.genres 
     @@genres 
   end
-  
+  def self.artists 
+    @@artists
+  end
+  def self.genre_count 
     
