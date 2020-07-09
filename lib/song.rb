d468e23eed483fe96046320291440f31b9fdd8d0
Sami  Sabeeh <samsabeeh6@gmail.com>
2020-07-09 02:33:12 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -15,10 +15,10 @@ class Song
     @@count 
   end
   def self.genres 
-    @@genres 
+    @@genres.uniq 
   end
   def self.artists 
-    @@artists
+    @@artists.uniq 
   end
   def self.genre_count
     genre_count = {}
