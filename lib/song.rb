69b219660c07d178164e99a62cf01eb7fc66dae4
Sami  Sabeeh <samsabeeh6@gmail.com>
2020-07-09 02:30:17 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -33,5 +33,6 @@ class Song
   end
   def self.artist_count
     artist_count = {}
-    @@artists.each do |artists|
-      if artist_count[art]
+    @@artists.each do |artist|
+      if artist_count[artist]
+        
