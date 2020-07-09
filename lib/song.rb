06f5f1010548bb85443fdb80d9059d5d7084a849
Sami  Sabeeh <samsabeeh6@gmail.com>
2020-07-09 02:23:42 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -22,5 +22,12 @@ class Song
   end
   def self.genre_count
     genre_count = {}
-    @@genres.each do 
-    
+    @@genres.each do |genre| 
+      if genre_count[genre]
+      genre_count[genre] += 1 
+      else
+      genre_count[genre] = 1
+      end
+    end
+    genre_count
+  end
